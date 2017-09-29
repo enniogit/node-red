360ec8eb71ffaf545b6fa5f3ad34242b3c121403
https://www.github.com/enniogit/node-red.git <ennio_picarelli@it.ibm.com>
2017-09-29 11:31:25 +0200

    mettiamo le cose a posto
    mettiamo le cose a posto

---
+++ eliminafile.cmd
@@ -0,0 +1,1 @@
+curl -X DELETE -u "cc558985-ee01-40ff-ba1e-466f830921e7":"s56l1s5W5NPd" "https://gateway.watsonplatform.net/discovery/api/v1/environments/656e65a1-32a6-45cc-860c-bd9f35ab4e4f/collections/53c3c4fb-3b55-4f5e-8598-f348776349b4/documents/713fcadac804b8a02a49caf2a94e03b8?version=2017-08-01"
